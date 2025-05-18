5dc97d19ecf2d17bb0d394ca93e2f4e994f27b76
Mathias Schindler <mathingvid@gmail.com>
2025-05-18 17:33:38 +0200

    move firebase credentials to private dotenv to hide

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const { initializeApp: initializeAdminApp } = require("firebase-admin/app");
-initializeAdminApp();
-
+require('dotenv').config();
 const { initializeApp } = require('firebase/app');
+const { initializeApp: initializeAdminApp } = require("firebase-admin/app");
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDZOYnEHUA3I1PS36teLMmKirTXpMVzFAc",
-    authDomain: "sale-check-b611b.firebaseapp.com",
-    projectId: "sale-check-b611b",
-    storageBucket: "sale-check-b611b.appspot.com",
-    messagingSenderId: "712429550284",
-    appId: "1:712429550284:web:3fdb2d4ab2e5b99448a069",
-    measurementId: "G-743W5CC4YL"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 initializeApp(firebaseConfig);
+initializeAdminApp();
 
 
 const {
