a46c2e741f4672c007ba10e2ea662e28ac71423a
Mathias Schindler <mathingvid@gmail.com>
2025-05-21 01:34:32 +0200

    restructure test files to match dev file locations and run through index.test.js

--- firestore/products/getProductToCheck.int.test.js
+++ firestore/products/getProductToCheck.int.test.js
@@ -14,50 +14,52 @@ const db = getFirestore();
 const app = express();
 app.use('/getProductToCheck', getProductToCheck);
 
-describe('GET /getProductToCheck', () => {
-  const testProductId = 'test-product-id'
-  const testProductData = {
-    "productName": "ExampleProduct",
-		"expectedPrice": 29.99,
-		"expectedPriceCurrency": "USD",
-		"url": "http: //example.com/product",
-		"emailNotification": [
-			"user1@example.com",
-			"user2@example.com"
-		],
-		"cssSelector": ".product-price",
-		"user": "YlGEGBCRfBV6o3TIUrTqvcdMxMi2"
-  }
+exports.getProductToCheckIntTest = function () {
+  describe('GET /getProductToCheck', () => {
+    const testProductId = 'test-product-id';
+    const testProductData = {
+      "productName": "ExampleProduct",
+      "expectedPrice": 29.99,
+      "expectedPriceCurrency": "USD",
+      "url": "http: //example.com/product",
+      "emailNotification": [
+        "user1@example.com",
+        "user2@example.com"
+      ],
+      "cssSelector": ".product-price",
+      "user": "YlGEGBCRfBV6o3TIUrTqvcdMxMi2"
+    };
 
-  before(async () => {
-    await db.collection("productsToCheck").doc(testProductId).set(testProductData);
-  })
+    before(async () => {
+      await db.collection("productsToCheck").doc(testProductId).set(testProductData);
+    });
 
-  after(async () => {
-    await db.collection("productsToCheck").doc(testProductId).delete();
-  })
+    after(async () => {
+      await db.collection("productsToCheck").doc(testProductId).delete();
+    });
 
-  it('should return 200 and the product snapshot data if it exists', async () => {
-    const res = await request(app)
-      .get('/getProductToCheck')
-      .query({ id: testProductId })
+    it('should return 200 and the product snapshot data if it exists', async () => {
+      const res = await request(app)
+        .get('/getProductToCheck')
+        .query({ id: testProductId })
 
       expect(res.status).to.equal(200);
       expect(res.body).to.deep.equal(testProductData)
-  });
+    });
 
-  it("should return 400 if param id param is missing from payload", function() {
-    console.warn("⚠️ Still TBA:");
-    this.skip();
-  });
+    it("should return 400 if param id param is missing from payload", function () {
+      console.warn("⚠️ Still TBA:");
+      this.skip();
+    });
 
-  it("should return 404 if product does not exist", function() {
-    console.warn("⚠️ Still TBA.");
-    this.skip();
-  });
+    it("should return 404 if product does not exist", function () {
+      console.warn("⚠️ Still TBA.");
+      this.skip();
+    });
 
-  it("should return 405 if req method is not GET", function() {
-    console.warn("⚠️ Still TBA.");
-    this.skip();
+    it("should return 405 if req method is not GET", function () {
+      console.warn("⚠️ Still TBA.");
+      this.skip();
+    });
   });
-});
+};
--- index.test.js
+++ index.test.js
@@ -1,22 +1,24 @@
-// require('dotenv').config();
-const {
-    expect,
-    assert
-} = require('chai');
+// // require('dotenv').config();
+// const {
+//     expect,
+//     assert
+// } = require('chai');
 
-const test = require("firebase-functions-test")();
+// const test = require("firebase-functions-test")();
 
-// const myFunctions = require("../index.js");
-const wrap = test.wrap;
-
-
-// describe('Environment and function loading', () => {
-//   it('should load environment variables and functions', () => {
-//     expect(process.env.PROJECT_ID).to.exist;
-//     expect(process.env.STORAGE_BUCKET).to.exist;
-//     // expect(myFunctions).to.exist;
-//   });
-// });
+// // const myFunctions = require("../index.js");
+// const wrap = test.wrap;
 
 
+// // describe('Environment and function loading', () => {
+// //   it('should load environment variables and functions', () => {
+// //     expect(process.env.PROJECT_ID).to.exist;
+// //     expect(process.env.STORAGE_BUCKET).to.exist;
+// //     // expect(myFunctions).to.exist;
+// //   });
+// // });
+
+const { getProductToCheckIntTest } = require('./firestore/products/getProductToCheck.int.test');
+
+getProductToCheckIntTest();
   
