d4f8a5fe2d2385398090e09ef1a3a06784500296
Mathias Schindler <mathingvid@gmail.com>
2025-05-25 17:08:16 +0200

    ensure auth emulator connection handling

--- auth/createUser.js
+++ auth/createUser.js
@@ -1,39 +1,51 @@
 const { onRequest } = require("firebase-functions/v2/https");
-const { 
-    getAuth, 
-    createUserWithEmailAndPassword, 
-    updateProfile ,
-    sendEmailVerification
+const {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    sendEmailVerification,
+    connectAuthEmulator
 } = require("firebase/auth");
 const cors = require('cors')({ origin: true });
 
 
 exports.createUser = onRequest({ timeoutSeconds: 300, memory: "1GiB" }, async (req, res) => {
-    cors(req, res, async () => {    
+    cors(req, res, async () => {
         if (req.method !== "POST") return res.status(405).send({ success: false, error: 'Method Not Allowed. Only POST requests are allowed.' });
         if (req.get('Content-Type') !== 'application/json') {
             return res.status(400).send({ success: false, error: 'Content-Type must be application/json.' });
         }
-        
+
         const { email, pwd, displayName, photoURL } = req.body.data;
         if (!email || !pwd) {
             return res.status(400).send({ error: 'Bad Request: Email and password are required' });
         }
-        
+
         const auth = getAuth();
+        if (process.env.FUNCTIONS_EMULATOR === 'true') {
+            console.log('Active emulator instance detected. Connecting to Auth emulator ...');
+            try {
+                connectAuthEmulator(auth, "http://localhost:9099");
+                console.log('... Auth emulator connection successfully established');
+            } catch {
+                console.error('Failed to connect to Auth emulator:', err);
+                throw new Error('Failed to connect to Auth emulator');
+            }
+        }
+
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, pwd);
             const user = userCredential.user;
 
             await sendEmailVerification(auth.currentUser);
-            
+
             await updateProfile(user, {
                 displayName: displayName || '',
                 photoURL: photoURL || '',
             })
-            
+
             return res.status(201).send({ status: "Success", user: user });
-        
+
         } catch (error) {
             console.error("Error creating user: ", error);
             return res.status(500).send({ status: 'Internal Server Error', error: error });
--- auth/getUser.js
+++ auth/getUser.js
@@ -2,21 +2,32 @@ const { onRequest } = require("firebase-functions/v2/https");
 const { getAuth } = require("firebase-admin/auth");
 const cors = require('cors')({ origin: true });
 
-exports.getUser = onRequest( async (req, res) => {
-    cors(req, res, async () => {    
+if (process.env.FUNCTIONS_EMULATOR === 'true') {
+    console.log('Active emulator instance detected. Connecting to Auth emulator ...');
+    try {
+        process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
+        console.log('... Auth emulator connection successfully established');
+    } catch {
+        console.error('Failed to connect to Auth emulator:', err);
+        throw new Error('Failed to connect to Auth emulator');
+    }
+}
+
+exports.getUser = onRequest(async (req, res) => {
+    cors(req, res, async () => {
         if (req.method !== "GET") return res.status(405).send({ success: false, error: 'Method Not Allowed. Only GET requests are allowed.' });
 
         const uid = req.query.uid;
-        if (!uid) return res.status(400).send({ success: false, error: "Bag Request: 'uid' is required in the query parameters." });          
+        if (!uid) return res.status(400).send({ success: false, error: "Bag Request: 'uid' is required in the query parameters." });
 
         getAuth()
-        .getUser(uid)
-        .then((userRecord) => {
-            res.status(200).send({ status: "Success", user: userRecord.toJSON() });
-        })
-        .catch((error) => {
-            console.error("Error fetching user data:", error);
-            res.status(500).send({ status: 'Internal Server Error', error: error });
-        });
+            .getUser(uid)
+            .then((userRecord) => {
+                res.status(200).send({ status: "Success", user: userRecord.toJSON() });
+            })
+            .catch((error) => {
+                console.error("Error fetching user data:", error);
+                res.status(500).send({ status: 'Internal Server Error', error: error });
+            });
     })
 });
