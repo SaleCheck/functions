a9a6bfc1e34d0f8b3d8251056d23a84d67d5534e
Mathias Schindler <mathingvid@gmail.com>
2025-06-07 11:18:11 +0200

    refactor to set appropriate env vars to firebase emulators dynamically from firebase.json in root

--- index.test.js
+++ index.test.js
@@ -1,10 +1,13 @@
-const admin = require('firebase-admin');
 const firebaseConfig = require('../firebase.json');
+if (firebaseConfig.emulators && firebaseConfig.emulators.firestore) process.env.FIRESTORE_EMULATOR_HOST = `localhost:${firebaseConfig.emulators.firestore.port}`;
+if (firebaseConfig.emulators && firebaseConfig.emulators.auth) process.env.FIREBASE_AUTH_EMULATOR_HOST = `localhost:${firebaseConfig.emulators.auth.port}`;
+if (firebaseConfig.emulators && firebaseConfig.emulators.storage) process.env.FIREBASE_STORAGE_EMULATOR_HOST = `localhost:${firebaseConfig.emulators.storage.port}`;
 
 if (!process.env.FIRESTORE_EMULATOR_HOST) throw new Error('FIRESTORE_EMULATOR_HOST is not set');
-if (firebaseConfig.emulators && firebaseConfig.emulators.storage) {
-    process.env.FIREBASE_STORAGE_EMULATOR_HOST = `localhost:${firebaseConfig.emulators.storage.port}`;
-}
+if (!process.env.FIREBASE_AUTH_EMULATOR_HOST) throw new Error('FIREBASE_AUTH_EMULATOR_HOST is not set');
+if (!process.env.FIREBASE_STORAGE_EMULATOR_HOST) throw new Error('FIREBASE_STORAGE_EMULATOR_HOST is not set');
+
+const admin = require('firebase-admin');
 if (!admin.apps.length) {
     admin.initializeApp({
         projectId: process.env.PROJECT_ID,
